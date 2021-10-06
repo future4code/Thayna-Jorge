a05451dff609b6313fd5660bc8cec45103b43081
Thayna Jorge <thaynasaad.ts@gmail.com>
2021-10-05 23:39:30 -0300

    Consegui fazer os exercicios de hoje

---
+++ modulo1/operadores/template_javascript/index.js
@@ -0,0 +1,63 @@
+//Exercício de interpretação de código
+// 1- Primeiro exercício
+//Resultado de "a" vai ser falso
+//Resultado de "b" vai ser falso
+//Resultado de "c" vai ser verdadeiro
+// "d" vai mostrar o resultado
+
+//2 - Percebi que o as variáveis estão sem Number antecedendo elas
+// 3- Pra resolver seria por o Number antes e ficaria (Number(variavel))
+
+
+// Exercício de escrita de código
+
+//1- a) Pergunte a idade do usuário
+//const idade = prompt ("Qual sua idade?")
+//b) Pergunte a idade do seu melhor amigo ou da sua melhor amiga
+//const idadeAmig = prompt ("Qual a idade do seu melhro amigo?")
+//let result = (Number (idade) > Number(idadeAmig))
+
+//c) **Imprima na console** a seguinte mensagem: "Sua idade é maior do que a do seu melhor amigo?", seguido pela resposta (`true` ou `false`)
+//console.log("Sua idade é maior que a idade do seu melhor amigo?", result)
+//d) **Imprima na console** a diferença de idade (não tem problema se sair um número negativo)
+//let result1 = (Number (idade)- Number (idadeAmig))
+//console.log("Você e seu amigo tem", result1, " anos de diferença de idade")
+
+// 2 Faça um programa que:
+//a) Peça ao usuário que insira um número **par**
+//const numPar = prompt ("Digite um número par")
+//b) Imprima na console **o resto da divisão** desse número por 2.
+//console.log(Number (numPar) % 2)
+//c) Teste o programa com diversos números pares. Você notou um padrão? Escreva em um comentário de código.
+// O resultado retorna zero
+//d) O que acontece se o usuário inserir um número ímpar? Escreva em um comentário de código
+/** O console vai imprimir o resto da divisão desse número por 2, ex: 7/2 = 6 resta 1 */
+
+//3. Faça um programa que pergunte ao usuário sua idade em anos. Depois, imprima no console 
+//const idadeUsu = prompt ("Em qeu ano você nasceu ?")
+
+// a) A idade do usuário em meses
+//console.log("Você tem", (Number (idadeUsu)* 12), "anos em meses" )   
+//b) A idade do usuário em dias
+//console.log("Você tem", (Number (idadeUsu)* 365), "anos em dias" )   
+//c) A idade do usuário em horas
+//console.log("Você tem", (Number (idadeUsu)* 8760), "anos em horas" )   
+
+// Exercício 4
+//Faça um programa que pergunte ao usuario dois numeros
+// faça as operações 
+const primeiroN = prompt ("Digite o primeiro número")
+const segundoN = prompt ("Digite o segundo número")
+// imprima no console true ou false
+//O primeiro numero é maior que segundo? true
+console.log(Number(primeiroN) > Number(segundoN))
+
+//O primeiro numero é igual ao segundo? false
+console.log(Number(primeiroN) === Number(segundoN))
+
+//O primeiro numero é divisível pelo segundo? true
+//const sobre = (Number(primeiroN) %  Number(segundoN))
+//console.log(sobra === 0)
+//O segundo numero é divisível pelo primeiro? true
+//const sobra = (Number(segundoN) %  Number(primeiroN))
+//console.log(sobra === 0)
