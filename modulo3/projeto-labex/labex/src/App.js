481422072bad2d0aa5df400ab63c70d2d76363c6
thayna-saad <thaynasaad.ts@gmail.com>
2021-12-06 18:06:59 -0300

    Projeto LabeX- Entrega do dia - Mook e criação de telas VScode

---
+++ modulo3/projeto-labex/labex/src/App.js
@@ -0,0 +1,30 @@
+import react from "react";
+import axios from "axios";
+import {useState, useEffect} from react
+
+
+ export const App = () => {
+
+
+
+
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
